bd51067256dc3ddeff999e23a455c73e56e86017
AWells595 <84533265+AWells595@users.noreply.github.com>
2022-12-07 12:56:15 -0500

    day 7 struggles part 1

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,14 @@
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Driver {
     public static void main(String[] args) throws FileNotFoundException {
         final long startTime = System.nanoTime();
-        SignalPacket packet = new SignalPacket();
-        packet.readFile();
-        System.out.println(packet.countToStartOfPacket(14));
+        Directory directory = new Directory();
+        ArrayList<String> fileContents = new ArrayList<>(directory.readFile());
+        directory.listOfDirectories(fileContents);
+        directory.directoryContents(fileContents);
         final long duration = System.nanoTime() - startTime;
         System.out.println(duration/1000000.0);
     }
